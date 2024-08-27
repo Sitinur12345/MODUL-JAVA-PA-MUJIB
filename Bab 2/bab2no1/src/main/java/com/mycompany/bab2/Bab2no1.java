306948ce8b4bab40fe5e5918c0592ed32e7d0b89
Sitinur12345 <uraenist@gmail.com>
2024-08-27 17:46:09 +0700

    Add files via upload

---
+++ Bab 2/bab2no1/src/main/java/com/mycompany/bab2/Bab2no1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2;
+
+/**
+ *
+ * @author halimah
+ */
+public class Bab2no1 {
+
+    public static void main(String[] args) {
+        int a=50;
+        float b=a;
+        System.out.println(a);
+        System.out.println(b); 
+    }
+}
---
+++ Bab 2/bab2no2/src/main/java/com/mycompany/bab2no2/Bab2no2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2no2;
+
+/**
+ *
+ * @author halimah
+ */
+public class Bab2no2 {
+    public static String Makanan;
+//Membuat variable tanpa Value/Nilai pada class staticvariable
+    public static final String Minuman = "susu strawberry";
+    public static void main(String[] args) {
+// TODO code application logic here
+    Makanan = "Mie Ayam ";//Mengisi Value pada variable Makanan
+      System.out.println("Makanan Favorit Saya  " + 
+    Makanan);//Memanggil Variable Makanan
+      System.out.println("Minuman Favorit Saya " + 
+    Minuman);//Memanggil Variable Minuman
+    }
+}
